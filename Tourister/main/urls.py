086c373de7da9e6390360c90e3bb9776190b6560
MOH7117 <mohammad201027@hotmail.com>
2023-02-05 11:46:06 +0300

    lab cookies

--- Tourister/main/urls.py
+++ Tourister/main/urls.py
@@ -4,12 +4,11 @@ from . import views
 app_name = "main"
 
 urlpatterns = [
-    path('',views.dmm, name="dmm"),
+    path('home/',views.home, name="home"),
     path('Abha/',views.abha, name="city/Abha"),
     path('Mekkah/',views.mekkah, name="city/Mekkah"),
     path('Riyadh/',views.riyadh, name="city/Riyadh"),
     path('AlUla/',views.alula, name="city/AlUla"),
-    path("consent/tos/", views.consent_TOS, name="consent_to_tos"),
     path("mode/dark/", views.light_mode, name="mode_dark"),
     path("mode/light/", views.dark_mode, name="mode_light"),
 ]
